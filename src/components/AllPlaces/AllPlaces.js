75de5b99994608aef759a09b74ad3bba22d1d345
Ioannis Bregiannis <i.bregiannis@gmail.com>
2022-11-06 13:17:28 +0200

    css fixes

--- src/components/AllPlaces/AllPlaces.js
+++ src/components/AllPlaces/AllPlaces.js
@@ -2,11 +2,12 @@ import React from 'react';
 // @ts-ignore
 import places from '../../assets/Places.json';
 import { Place } from '../Place/Place.js';
+import './AllPlaces.css';
 
 export function AllPlaces() {
     const placeArray = places;
     return (
-        <div>
+        <div className='flex'>
             {
                 placeArray.map(
                     item => <Place placeInfo={item} key={item.index}></Place>
