716b77a8c1cd75350aa56b7a94900e655b2fa830
Ioannis Bregiannis <i.bregiannis@gmail.com>
2022-11-06 13:23:37 +0200

    more fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,48 +1,46 @@
-import React from 'react';
-import './Navbar.css';
-import styled from 'styled-components';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-import App from '../../App';
-import { AllPlaces } from '../AllPlaces/AllPlaces';
-import { MyPlaces } from '../MyPlaces/MyPlaces';
+import React from "react";
+import "./Navbar.css";
+import styled from "styled-components";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import App from "../../App";
+import { AllPlaces } from "../AllPlaces/AllPlaces";
+import { MyPlaces } from "../MyPlaces/MyPlaces";
 
 export function NavBar() {
-    const NavButton = styled.div`
-	:hover {
-		color: white;
-        cursor: pointer;
+  const NavButton = styled.div`
+    :hover {
+      color: white;
+      cursor: pointer;
     }
-	`;
-    return (
-        <Router>
-            <div className="navBar">
-                <ul>
-                    <Link to='/'><li><NavButton className="navItem brand">Accessibility</NavButton></li></Link>
-                    <Link to='/allPlaces'><li><NavButton className="navItem">All Places</NavButton></li></Link>
-                    <Link to='/myPlaces'><li><NavButton className="navItem">My Places</NavButton></li></Link>
-                    <li className="searchBar">
-                        <label>Search:</label>
-                        <input className="inputNav"></input>
-                    </li>
-                </ul>
-
-            </div>
-            <Switch>
-          <Route path="/home">
-            <App />
-          </Route>
-          <Route path="/allPlaces">
-            <AllPlaces />
-          </Route>
-          <Route path="/myPlaces">
-            <MyPlaces/>
-          </Route>
-        </Switch>
-        </Router>
-    )
+  `;
+  return (
+    <Router>
+      <div className="navBar">
+        <Link to="/">
+          <NavButton className="navItem brand">Accessibility</NavButton>
+        </Link>
+        <Link to="/allPlaces">
+          <NavButton className="navItem">All Places</NavButton>
+        </Link>
+        <Link to="/myPlaces">
+          <NavButton className="navItem">My Places</NavButton>
+        </Link>
+        <div className="searchBar">
+          <label>Search:</label>
+          <input className="inputNav"></input>
+        </div>
+      </div>
+      <Switch>
+        <Route path="/home">
+          <App />
+        </Route>
+        <Route path="/allPlaces">
+          <AllPlaces />
+        </Route>
+        <Route path="/myPlaces">
+          <MyPlaces />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
